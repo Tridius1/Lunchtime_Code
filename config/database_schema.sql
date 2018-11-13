3545ed932af9adb5aba079b74b46fe2d1104be2a
Tridius1 <tristan.t73@gmail.com>
2018-11-12 18:02:21 -0700

    Updated schema and test data

--- config/database_schema.sql
+++ config/database_schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "users" (
 	"id" serial NOT NULL,
-	"username" TEXT NOT NULL UNIQUE,
-	"password" bit(256),
+	"email" TEXT NOT NULL UNIQUE,
+	"user" TEXT NOT NULL UNIQUE,
+	"password" TEXT,
 	"Watchlist" varchar(4) ARRAY,
 	CONSTRAINT Users_pk PRIMARY KEY ("id")
 ) WITH (
