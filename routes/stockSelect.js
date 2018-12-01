3eba22e18b4b69b018e72de6ce257f814b7b9660
Valyria McFarland <vamc0256@colorado.edu>
2018-11-30 19:38:36 -0700

    updated stockSelect.js

--- routes/stockSelect.js
+++ routes/stockSelect.js
@@ -8,42 +8,6 @@ app.get('/', function(req, res){
 
 module.exports = app;
 
-
-var http = require('http');
-
-var options = {
-	host: 'https://api.iextrading.com/1.0/stock/aapl/logo',
-	method: 'GET'
-};
-
-function getJSON(options, cb){
-	http.request(options,function(response){
-		var body = "";
-		
-		response.on('data', function(some_data){
-			body += some_data;
-		});
-
-		response.on('end', function(){
-			var result = JSON.parse(body)
-			cb(null, result);
-		});
-
-		response.on('error', cb);
-	})
-	.on('error', cb)
-	.end();
-}
-
-var url = "";
-
-getJSON(options, function(err, result){
-	if(err){
-		//error
-		return
-	}
-	url = result;
-});
     
 
 
