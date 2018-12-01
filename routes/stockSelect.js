f2aa4dbcb15890b17ed7e023a2e5c40d5c587c66
Valyria McFarland <vamc0256@colorado.edu>
2018-11-30 19:20:35 -0700

    modified select stock page

--- routes/stockSelect.js
+++ routes/stockSelect.js
@@ -9,41 +9,67 @@ app.get('/', function(req, res){
 module.exports = app;
 
 
-var http = require('http');
+var company = {}
 
-var options = {
-	host: 'https://api.iextrading.com/1.0/stock/aapl/logo',
-	method: 'GET'
-};
+    var symb = []
+    var company_names = []
 
-function getJSON(options, cb){
-	http.request(options,function(response){
-		var body = "";
-		
-		response.on('data', function(some_data){
-			body += some_data;
-		});
+    $(document).ready(function() {
+        var most_active_url = 'https://api.iextrading.com/1.0/stock/market/list/mostactive'
+        $.ajax({url:most_active_url, dataType:"jsonp"}).then(function(data) {
 
-		response.on('end', function(){
-			var result = JSON.parse(body)
-			cb(null, result);
-		});
+            //populate symb with symbols and company_names with names of 10 most active companies
+            var i;
+            for(i = 0 ; i < data.length; i++){
+                symb.push(data[i].symbol)
+                //cut off company name if it exceeds 25 characters
+                var letter_limit = 25;
+                if(data[i].companyName.length > letter_limit){
+                    var new_str
+                    var n
+                    for(n = letter_limit; n > 0; n--){
+                        if(data[i].companyName.charAt(n) == ' '){
+                            new_str = data[i].companyName.substring(0,n)
+                            break
+                        }
+                    }
+                    company_names.push(new_str)
+                }else{
+                    
+                    company_names.push(data[i].companyName)
+                }
+                //populate company object
+                company[symb[i]] = company_names[i];
 
-		response.on('error', cb);
-	})
-	.on('error', cb)
-	.end();
-}
+            }
 
-var url = "";
 
-getJSON(options, function(err, result){
-	if(err){
-		//error
-		return
-	}
-	url = result;
-});
+            var num = 1
+            Object.keys(company).forEach(function(key) {
+                //key is the company's symbol
+                //value is the company's name
+                var value = company[key];
+                console.log(key);
+                   
+                var str_logo = '#results' + num
+
+                var str = '#name' + num
+                $(str).text(value)
+
+                var url ='https://api.iextrading.com/1.0/stock/' + key + '/logo'
+                $.ajax({url:url, dataType:"jsonp"}).then(function(data_url) {
+                    $(str_logo).css({'background-image' : 'url(' + data_url.url + ')', 'background-repeat' : 'no-repeat', 'background-position' : 'center'});
+                });
+                num++
+                
+            });
+            
+  
+        })
+   
+    })
+    
+
 
 
 
